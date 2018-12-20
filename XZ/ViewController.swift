667a7bb91529b2a25be18c91976ff900dee7c07b
巫嘉喆 <shaogonhuang@foxmail.com>
2018-12-20 18:33:19 +0800

    Initial Commit

---
+++ XZ/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  XZ
+//
+//  Created by wjz on 2018/12/20.
+//  Copyright © 2018年 wjz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
